6ade0a7c6d1f8fba5318395f0f9936285015c39e
Vic Iglesias <viglesiasce@gmail.com>
2014-08-12 15:52:05 -0700

    Implement basic auth

--- resource_manager/client.py
+++ resource_manager/client.py
@@ -21,21 +21,22 @@ class ResourceManagerClient(object):
               'private-addresses': ["address", "owner", "_updated", "_id"],
               'public-addresses': ["address",  "owner", "_updated", "_id"]}
 
-    def __init__(self, resource_type='machines', endpoint='http://127.0.0.1:5000'):
+    def __init__(self, resource_type='machines', endpoint='http://127.0.0.1:5000', username='admin', password='admin'):
         self.endpoint = endpoint + '/' + resource_type
         self.resource_type = resource_type
         self.key = self.FIELDS[self.resource_type][0]
+        self.auth = (username, password)
         self.headers = {'content-type': 'application/json'}
         self.fields = self.FIELDS[self.resource_type]
 
     def create_resource(self, resource):
-        resource = requests.post(self.endpoint, data=resource, headers=self.headers)
+        resource = requests.post(self.endpoint, data=resource, headers=self.headers, auth=self.auth)
         if resource.status_code != 201:
             raise RequestFailureException(resource)
 
     def get_resource(self, name):
         url = self.endpoint + '/' + urllib.quote_plus(name)
-        return requests.get(url).json()
+        return requests.get(url, auth=self.auth).json()
 
     def update_resource(self, resource):
         resource_name = json.loads(resource)[self.key]
@@ -45,7 +46,7 @@ class ResourceManagerClient(object):
         request_url = self.endpoint + "/" + identifier
         headers = self.headers
         headers['If-Match'] = etag
-        updated_resource = requests.patch(request_url, data=resource, headers=self.headers)
+        updated_resource = requests.patch(request_url, data=resource, headers=self.headers, auth=self.auth)
         if updated_resource.status_code != 200:
             raise RequestFailureException(updated_resource)
 
@@ -57,12 +58,12 @@ class ResourceManagerClient(object):
         request_url = self.endpoint + "/" + identifier
         headers = self.headers
         headers['If-Match'] = etag
-        deleted_resource = requests.delete(request_url, data=resource, headers=self.headers)
+        deleted_resource = requests.delete(request_url, data=resource, headers=self.headers, auth=self.auth)
         if deleted_resource.status_code != 200:
             raise RequestFailureException(deleted_resource)
 
     def print_resources(self):
-        items = requests.get(self.endpoint).json()["_items"]
+        items = requests.get(self.endpoint, auth=self.auth).json()["_items"]
         table = PrettyTable(self.fields)
         for resource in items:
             table.add_row([resource.get(field, None) for field in self.fields])
--- resource_manager/server.py
+++ resource_manager/server.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 from eve import Eve
+from eve.auth import BasicAuth
 from flask_bootstrap import Bootstrap
 from eve_docs import eve_docs
-app = Eve(settings="./api_config.py")
+
+class ResourceManagerBasicAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource, method):
+        if method == 'GET':
+            # Allow read-only access without a password
+            return True
+        else:
+            # all the other resources are secured
+            return username == 'admin' and password == 'admin'
+
+app = Eve(settings="./api_config.py", auth=ResourceManagerBasicAuth)
 Bootstrap(app)
 app.register_blueprint(eve_docs, url_prefix='/docs')
 app.run(host='0.0.0.0')
