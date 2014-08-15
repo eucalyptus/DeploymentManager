a33cf1d6868fc37e02ece3c9cd661eda72adae78
Tony Beckham <tony.beckham@gmail.com>
2014-08-15 15:38:20 -0700

    Some Debug crap

--- pxe_manager/pxemanager.py
+++ pxe_manager/pxemanager.py
@@ -35,7 +35,6 @@ import xmlrpclib
 import json
 
 from time import sleep
-from resource_manager.client import ResourceManagerClient
 from paramiko import BadHostKeyException, AuthenticationException, SSHException, SSHClient, AutoAddPolicy
 
 
@@ -60,6 +59,11 @@ class PxeManager(object):
         :return:
         """
         resources = self.resource_manager.find_resources(field="state", value="idle")['_items']
+        '''
+        debug
+        '''
+        print "PM count: " + count
+        print "PM resource count: " + str(len(resources))
         if len(resources) < count:
             print "Oops...There are not enough free resources to fill your request."
             return
