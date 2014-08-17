763a7eb12dbfade739ae19c8f937da29807e3bf3
Vic Iglesias <viglesiasce@gmail.com>
2014-08-17 00:38:42 -0700

    Schema unit tests

--- resource_manager/api_config.py
+++ resource_manager/api_config.py
@@ -3,66 +3,68 @@ ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 PAGINATION = False
 lookup_url = 'regex("[\w-]+")'
 
-machine_schema = {
-    'hostname': {
-        'type': 'string',
-        'required': True,
-        'unique': True
+class ResourceSchema(object):
+    machine_schema = {
+        'hostname': {
+            'type': 'string',
+            'required': True,
+            'unique': True
 
-    },
-    'owner': {
-        'type': 'string'
-    },
-    'state': {
-        'type': 'string',
-        'required': True,
-        'allowed': ["pxe", "pxe_failed", "idle", "in_use", "needs_repair"]
-    },
-    'job_id': {
-        'type': 'string'
+        },
+        'owner': {
+            'type': 'string'
+        },
+        'state': {
+            'type': 'string',
+            'required': True,
+            'allowed': ["pxe", "pxe_failed", "idle", "in_use", "needs_repair"]
+        },
+        'job_id': {
+            'type': 'string'
+        }
     }
-}
 
-address_schema = {
-    'address': {
-        'type': 'string',
-        'required': True,
-        'unique': True,
-    },
-    'owner': {
-        'type': 'string'
+    address_schema = {
+        'address': {
+            'type': 'string',
+            'required': True,
+            'unique': True,
+        },
+        'owner': {
+            'type': 'string'
+        }
     }
-}
 
-machines = {
-    'item_title': 'machine',
-    'additional_lookup': {
-        'url': lookup_url,
-        'field': 'hostname'
-    },
-    'schema': machine_schema
-}
+    machines = {
+        'item_title': 'machine',
+        'additional_lookup': {
+            'url': lookup_url,
+            'field': 'hostname'
+        },
+        'schema': machine_schema
+    }
 
-public_addresses = {
-    'item_title': 'public-address',
-    'additional_lookup': {
-        'url': lookup_url,
-        'field': 'address'
-    },
-    'schema': address_schema
-}
+    public_addresses = {
+        'item_title': 'public-address',
+        'additional_lookup': {
+            'url': lookup_url,
+            'field': 'address'
+        },
+        'schema': address_schema
+    }
 
-private_addresses = {
-    'item_title': 'private-address',
-    'additional_lookup': {
-        'url': lookup_url,
-        'field': 'address'
-    },
-    'schema': address_schema
-}
+    private_addresses = {
+        'item_title': 'private-address',
+        'additional_lookup': {
+            'url': lookup_url,
+            'field': 'address'
+        },
+        'schema': address_schema
+    }
 
+schema = ResourceSchema()
 DOMAIN = {
-    'machines': machines,
-    'public-addresses': public_addresses,
-    'private-addresses': private_addresses
+    'machines': schema.machines,
+    'public-addresses': schema.public_addresses,
+    'private-addresses': schema.private_addresses
 }
