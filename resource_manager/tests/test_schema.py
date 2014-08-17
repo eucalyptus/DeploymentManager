98c930b380d90038e27ab8ef5038a1b4918b2616
Vic Iglesias <viglesiasce@gmail.com>
2014-08-17 00:51:36 -0700

    Add states test

--- resource_manager/tests/test_schema.py
+++ resource_manager/tests/test_schema.py
@@ -26,6 +26,10 @@ def test_machine_unique():
     schema = ResourceSchema().machine_schema
     assert schema['hostname']['unique']
 
+def test_machine_states():
+    schema = ResourceSchema().machine_schema
+    assert schema["states"]['allowed'] == ["pxe", "pxe_failed", "idle", "in_use", "needs_repair"]
+
 
 def test_address_keys():
     schema = ResourceSchema().address_schema
