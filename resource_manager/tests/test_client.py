0b145254f43b4056652fb941492994c97d24e5a0
Vic Iglesias <viglesiasce@gmail.com>
2014-08-17 15:41:28 -0700

    Client tests PEP8

--- resource_manager/tests/test_client.py
+++ resource_manager/tests/test_client.py
@@ -4,24 +4,24 @@ import urllib
 
 from resource_manager.client import ResourceManagerClient
 
+
 def test_init():
-    client = ResourceManagerClient()
+    ResourceManagerClient()
+
 
 def test_defaults(resource='machines'):
     client = ResourceManagerClient(resource)
     assert client.resource_type == resource
-    defaults = { 'endpoint': 'http://127.0.0.1:5000/machines',
-                 'resource_type': 'machines',
-                 'key': 'hostname',
-                 'auth': ('admin', 'admin'),
-                 'headers': {'content-type': 'application/json'},
-                 'fields': ["hostname", "owner", "state", "job_id", "_updated", "_id"]}
+    defaults = dict(endpoint='http://127.0.0.1:5000/machines', resource_type='machines', key='hostname',
+                    auth=('admin', 'admin'), headers={'content-type': 'application/json'},
+                    fields=["hostname", "owner", "state", "job_id", "_updated", "_id"])
     for attribute, value in defaults.iteritems():
         try:
             assert value == client.__getattribute__(attribute)
-        except AssertionError as error:
+        except AssertionError:
             raise AssertionError("Default for {0} should be:{1}\n"
                                  "Received: {2}".format(attribute, client.__getattribute__(attribute), value))
+
 
 @httpretty.activate
 def test_get():
@@ -32,12 +32,14 @@ def test_get():
                            body=response_body)
     assert client.get_resource(resource) == json.loads(response_body)
 
+
 @httpretty.activate
 def test_create():
     client = ResourceManagerClient()
     resource = "{\"hostname\":\"resource\"}"
     httpretty.register_uri(httpretty.POST, client.endpoint, status=201)
     client.create_resource(resource)
+
 
 @httpretty.activate
 def test_delete():
@@ -51,6 +53,7 @@ def test_delete():
                            body=response_body)
     client.delete_resource(resource)
 
+
 @httpretty.activate
 def test_update():
     client = ResourceManagerClient()
@@ -63,6 +66,7 @@ def test_update():
                            body=response_body)
     client.update_resource(resource)
 
+
 @httpretty.activate
 def test_find_resources():
     client = ResourceManagerClient()
@@ -73,11 +77,10 @@ def test_find_resources():
     httpretty.register_uri(httpretty.GET, url, body=response_body)
     client.find_resources(field, value)
 
+
 @httpretty.activate
-def test_find_resources():
+def test_get_all_resources():
     client = ResourceManagerClient()
-    field = 'some_field'
-    value = 'some_value'
     url = client.endpoint
     response_body = "{\"_items\":[]}"
     httpretty.register_uri(httpretty.GET, url, body=response_body)
