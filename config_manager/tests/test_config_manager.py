228563ba9cc1b7701087a405ff68f2d0e7daacdb
Vic Iglesias <viglesiasce@gmail.com>
2014-08-17 15:28:19 -0700

    Add config manager unit tests

---
+++ config_manager/tests/test_config_manager.py
@@ -0,0 +1,36 @@
+from config_manager.eucalyptus.network import Network
+from config_manager.eucalyptus.packages import Packages
+from config_manager.eucalyptus import Eucalyptus
+from config_manager.eucalyptus.topology import Topology
+from mock import mock_open
+
+def test_init():
+    eucalyptus = Eucalyptus()
+
+def test_setters():
+    eucalyptus = Eucalyptus()
+    log_level = "TEST-LEVEL"
+    eucalyptus.set_log_level(log_level)
+    assert eucalyptus.log_level == log_level
+
+def test_add_topology():
+    eucalyptus = Eucalyptus()
+    topology = Topology(name="Test Topo")
+    eucalyptus.add_topology(topology)
+    assert eucalyptus.topology.name is topology.name
+
+def test_add_package_config():
+    eucalyptus = Eucalyptus()
+    eucalyptus_repo = 'eucalyptus-repo'
+    euca2ools_repo = 'euca2ools-repo'
+    packages = Packages(eucalyptus_repo, euca2ools_repo)
+    eucalyptus.add_packages(packages)
+    assert eucalyptus.eucalyptus_repo == eucalyptus_repo
+    assert eucalyptus.euca2ools_repo == euca2ools_repo
+
+def test_add_network():
+    eucalyptus = Eucalyptus()
+    network_json = '{}'
+    network_json_file = mock_open()
+    network = Network(network_json_file, network_json)
+    eucalyptus.add_network(network)
